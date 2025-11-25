cb74ecda8ce0102a2c85ac728558990f22859e21
nickhr23i <nickhr23i@gmail.com>
2025-11-25 07:06:16 +0200

    slightly modified line simulation and added handling for basic hook states

---
+++ Assets/Scripts/Hook.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum HookState
+{
+    Empty,
+    Bait,
+    Fish
+}
+
+public class Hook : MonoBehaviour
+{
+    public HookState state= HookState.Empty;
+    public GameObject hookModel;
+    public GameObject baitModel;
+    public GameObject fishModel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hookModel.SetActive(true);
+        baitModel.SetActive(false);
+        fishModel.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(state==HookState.Empty)
+        {
+            if(other.gameObject.name=="Bait")
+            {
+                hookModel.SetActive(false);
+                baitModel.SetActive(true);
+                Destroy(other.gameObject);
+                state= HookState.Bait;
+            }
+        }
+        else if(state==HookState.Bait)
+        {
+            if(other.gameObject.name=="WaterPlaceholder")
+            {
+                baitModel.SetActive(false);
+                fishModel.SetActive(true);
+                state = HookState.Fish;
+            }
+        }
+    }
+
+
+
+
+}
